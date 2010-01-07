0135c0ca93fcf7efef60f36fd9a5cdf88f235c95
Scott Weeks <weeksie@NY-G-SWEEKS-MAC.local>
2010-01-07 16:41:24 -0500

    added some documentation comments

--- jquery.broadcaster.js
+++ jquery.broadcaster.js
@@ -1,4 +1,17 @@
-(function() {
+/*
+ * jQuery Broadcaster Plugin
+ * version: 0.02 (05-JAN-2010)
+ * 
+ * @author Scott Weeks
+ * 
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+(function($) {
+/**
+ * register() causes an element to listen for an arbitrary event.
+ */
    $.fn.register = function(evt, f) {
      $._subscribers      = $._subscribers || {};
      $._subscribers[evt] = $._subscribers[evt] || [];
@@ -7,9 +20,13 @@
      return $(this);
    };
 
+/**
+ * broadcast() alerts all listening elements that an
+ * event has fired and passes along an optional arbitrary data object.
+ */
    $.broadcast = function(evt, data) {
      $.each($._subscribers[evt], function() {
 	      this.trigger(evt, data);
 	    });
    };
- })();
+ })(jQuery);
